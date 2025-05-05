a6ae07bbc9415ce2afb0080dd5da3c2d6a92d511
The-Wee-Lad <113277834+The-Wee-Lad@users.noreply.github.com>
2025-05-05 18:05:07 +0530

    feat: add configuration management with dotenv and define config interface

---
+++ src/configAndConstants.ts
@@ -0,0 +1,44 @@
+import dotenv from 'dotenv';
+dotenv.config({
+  path: './.env'
+});
+
+
+// --------------------------------------
+// Configuration Types
+// --------------------------------------
+interface Config {
+  DATABASE_URL: string;
+  ACCESS_TOKEN_KEY: string;
+  REFRESH_TOKEN_KEY: string;
+  ACCESS_TOKEN_EXPIRES: string;
+  REFRESH_TOKEN_EXPIRES: string;
+  PORT: number;
+  NODE_ENV: 'development' | 'production' | 'staging';
+}
+
+// --------------------------------------
+// Configuration
+// --------------------------------------
+
+const config: Config = {
+  DATABASE_URL: process.env.DATABASE_URL!,
+  ACCESS_TOKEN_KEY: process.env.ACCESS_TOKEN_KEY!,
+  REFRESH_TOKEN_KEY: process.env.REFRESH_TOKEN_KEY!,
+  ACCESS_TOKEN_EXPIRES: process.env.ACCESS_TOKEN_EXPIRES!,
+  REFRESH_TOKEN_EXPIRES: process.env.REFRESH_TOKEN_EXPIRES!,
+  PORT: parseInt(process.env.PORT!, 10) || 3000,
+  NODE_ENV: process.env.NODE_ENV! as 'development' | 'production' | 'staging' || 'development',
+}
+
+
+// --------------------------------------
+// Constants
+// --------------------------------------
+const cookieOptions = {
+  secure: config.NODE_ENV === 'production',
+  httpOnly: true,
+}
+
+
+export { cookieOptions, config };
