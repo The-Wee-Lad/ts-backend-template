d051777fbc4339c0b4fda9c008f921fa0f5b1202
The-Wee-Lad <113277834+The-Wee-Lad@users.noreply.github.com>
2025-05-06 05:00:05 +0530

    generateTokens

---
+++ src/utils/generateTokens.ts
@@ -0,0 +1,43 @@
+import jwt from "jsonwebtoken";
+import { User } from "../../generated/prisma";
+import { config } from "../configAndConstants"
+import { prismaClient } from "../db";
+import ms from 'ms';
+import { ApiError } from "./ApiError";
+
+const generateTokens = async (user: Omit<User, 'password' | 'refreshToken'>) => {
+  const accessToken = jwt.sign(user, config.ACCESS_TOKEN_KEY,
+    {
+      expiresIn: (config.ACCESS_TOKEN_EXPIRES as ms.StringValue)
+    }
+  );
+  const refreshToken = jwt.sign(user, config.REFRESH_TOKEN_EXPIRES,
+    {
+      expiresIn: (config.REFRESH_TOKEN_EXPIRES as ms.StringValue)
+    }
+  );
+
+  const updatedUser = await prismaClient.user.updateManyAndReturn(
+    {
+      where: {
+        id: user.id
+      },
+      data: {
+        refreshToken: refreshToken
+      },
+      omit: {
+        password: true,
+        refreshToken: true
+      }
+    }
+  )
+
+  if (!updatedUser) {
+    throw new ApiError(500, 'DB Server error', 'DB_003');
+  }
+
+  return { accessToken, refreshToken };
+
+}
+
+export { generateTokens }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,5 +2,5 @@ import { ApiError } from './ApiError';
 import { asyncHandler } from './asyncHandler';
 import { ApiResponse } from './ApiResponse';
 import { generateName, generateNumber, generateOtp } from './generateRandomStuff';
-
-export { ApiError, ApiResponse, asyncHandler };
+import { generateTokens } from './generateTokens';
+export { ApiError, ApiResponse, asyncHandler, generateName, generateNumber, generateOtp, generateTokens };
